ec1a9bce5a527fb563acb2f339c6badbe669698f
madeena-beevi <madeena25beevi@gmail.com>
2016-09-25 13:24:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,6 +58,11 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter =0;
+app.get('/counter', function (req,res){
+    counter = counter +1;
+    res.send(counter.toString());
+});
 
 app.get('/article-one', function (req, res) {
     res.send(createTemplate(articleone));
