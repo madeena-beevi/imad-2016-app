0ccb35b6f5bfbf77ab8741aa9709916b3c13da46
madeena-beevi <madeena25beevi@gmail.com>
2016-09-25 12:58:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var = element = document.getElementById('body1');
+var element = document.getElementById('body1');
 element.innerHTML ='NEW VALUE';
 var img = document.getElementById('mg1');
 img.onclick = function() {
