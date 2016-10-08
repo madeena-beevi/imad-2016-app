ce9f13147b9b4f5166158fd58d6c7c6ab48950ad
madeena-beevi <madeena25beevi@gmail.com>
2016-10-08 14:00:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,7 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-var img = document.getElementById('img1');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft +'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight,50);
-    
+var button = document.getElementById('counter');
+var counter =0;
+button.onclick = function() {
+  counter = counter + 1;
+  var span = document.getElementById('counter');
+  span.innerHTML = counter.toString();
 };
-
