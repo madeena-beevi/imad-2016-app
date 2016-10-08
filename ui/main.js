a2445a5a11c1bd0cfa4c7abb96c3654065e6258c
madeena-beevi <madeena25beevi@gmail.com>
2016-10-08 15:32:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,12 +22,12 @@ button.onclick = function() {
 };
 
 var nameInput = document.getElementById('name');
-var name = name.Input.value;
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
     
-    var names = ['name1' , 'name2' , 'name3','name4'];
+    var names = ['name1','name2','name3','name4'];
     var list ='';
     for (var i=0; i<names.length; i++){
         list += '<li>' + names[i] + '</li>';
